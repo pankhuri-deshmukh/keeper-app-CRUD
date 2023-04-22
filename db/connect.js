c965d394829f5182600ac45da5a9b2d255e0eb41
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-04-22 19:18:39 +0530

    Updated

---
+++ db/connect.js
@@ -0,0 +1,22 @@
+ const mongoose = require('mongoose')
+ 
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(url, {
+       useNewUrlParser: true,
+       useCreateIndex: true,
+       useFindAndModify: false,
+       useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((error) => {
+        console.log('Error connecting to database: ' + error);
+    })
+}
+
+module.exports = connectDB
+ 
+ 
